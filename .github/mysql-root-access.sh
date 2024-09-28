e86e69888d250ab984318c3fd58b04ec87ddcb0b
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2024-09-28 23:40:14 +0100

    fdsa

---
+++ .github/mysql-root-access.sh
@@ -0,0 +1,90 @@
+
+# script is here to assist running docker compose with mysql and pma
+# in addition to just spinning those containers it also wait for 'healthy' state of mysql container
+# and set proper privileges for external connectivity
+# and created database defined in MYSQL_DB env var
+# it is also packed with checking that all necessary environments are present
+
+_SHELL="$(ps -p $$ -o comm=)"; # bash || sh || zsh
+_SHELL="$(basename ${_SHELL//-/})"
+case ${_SHELL} in
+  zsh)
+    _DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd -P )"
+    ;;
+  sh)
+    _DIR="$( cd "$( dirname "${0}" )" && pwd -P )"
+    ;;
+  *)
+    _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+    ;;
+esac
+
+ROOT="${_DIR}/.."
+
+cd "${ROOT}"
+
+ROOT="$(pwd)"
+
+ENV=".env"
+
+if [ ! -f "${ENV}" ]; then
+
+  echo "${0} error: ${ENV} doesn't exist"
+
+  exit 1
+fi
+
+source "${ENV}"
+
+require_non_empty_var "${0}" "PROJECT_NAME"
+
+require_non_empty_and_matching_var "${0}" "PHPMYADMIN_PORT" "^[[:digit:]]+$"
+
+require_non_empty_var "${0}" "MYSQL_DB"
+
+require_non_empty_var "${0}" "MYSQL_PORT"
+
+require_non_empty_var "${0}" "MYSQL_PASS"
+
+_MYSQL_DB_ENV_NAME="MYSQL_DB"
+
+# NOTICE !!!
+# NOTICE !!!
+# NOTICE !!!
+# NOTICE !!!
+# Generally it is possible that this functionality of swaping env vars containing target database might not be needed in CI/CD environment but let's leave it as is for now
+# after all by default MYSQL_DB env var will be used
+# NOTICE !!!
+# NOTICE !!!
+# NOTICE !!!
+# NOTICE !!!
+#
+#
+#                 so as a final result this script now is working only with MYSQL_DB, that's all.
+#                 and it will stay like this
+#
+#
+# if [ "${MYSQL_DB_CHANGE}" != "" ]; then
+#     _MYSQL_DB_ENV_NAME="${MYSQL_DB_CHANGE}"
+# fi
+
+echo "_MYSQL_DB_ENV_NAME >${_MYSQL_DB_ENV_NAME}<"
+
+_MYSQL_DB_ENV_VALUE="$(eval echo "\$${_MYSQL_DB_ENV_NAME}")"
+
+if [ "${_MYSQL_DB_ENV_VALUE}" = "" ]; then
+
+    echo "${0} error: _MYSQL_DB_ENV_NAME (specified by MYSQL_DB_CHANGE>${MYSQL_DB_CHANGE}< or regular MYSQL_DB) after evaluation to _MYSQL_DB_ENV_VALUE shouldn't be empty string"
+
+    exit 1
+fi
+
+echo "_MYSQL_DB_ENV_VALUE >${_MYSQL_DB_ENV_VALUE}<"
+
+# CONTAINER="${PROJECT_NAME}_mysql"
+CONTAINER="mysql_container" # that's fixed name from github action mysql service
+
+docker exec -i "${CONTAINER}" mysql -u ${MYSQL_USER} -p${MYSQL_PASS} -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASS}'; flush privileges;"
+
+docker exec -i "${CONTAINER}" mysql -u ${MYSQL_USER} -p${MYSQL_PASS} -e "CREATE DATABASE IF NOT EXISTS ${_MYSQL_DB_ENV_VALUE} /*\!40100 DEFAULT CHARACTER SET utf8 */"
+
---
+++ .github/mysql-test.js
@@ -0,0 +1,105 @@
+// const path = require("path");
+import path from "path";
+
+// const fs = require("fs");
+import fs from "fs";
+
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+import knex from "knex";
+
+import { fileURLToPath } from "url";
+
+import getDbName from "../getDbName.cjs";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const env = path.resolve(__dirname, "..", ".env");
+
+// dotenv.config({
+//   path: env,
+// });
+
+// const fsPromises = require("node:fs/promises"); // not available in node v 15
+
+const th = (msg) => new Error(`mysql-test.js error: ${msg}`);
+
+const log = msg => process.stdout.write(`${msg}\n`);
+
+async function notExist(file) {
+  try {
+    await fsPromises.stat(file);
+    return false;
+  } catch (e) {
+    return e;
+  }
+}
+
+(async () => {
+  try {
+    // {
+    //   const nExist = await notExist(env);
+
+    //   if (nExist) {
+    //     throw th(`file '${env}' doesn't exist (${nExist.message})`);
+    //   }
+    // }
+
+    if (!fs.existsSync(env)) {
+      throw th(`file '${env}' doesn't exist (${nExist.message})`);
+    }
+
+    // require("dotenv").config({ path: env });
+    dotenv.config({
+      path: env,
+    });
+
+    if (typeof process.env.MYSQL_HOST !== "string") {
+      throw th(`MYSQL_HOST is undefined`);
+    }
+
+    if (typeof process.env.MYSQL_PORT !== "string") {
+      throw th(`MYSQL_PORT is undefined`);
+    }
+
+    if (typeof process.env.MYSQL_USER !== "string") {
+      throw th(`MYSQL_USER is undefined`);
+    }
+
+    if (typeof process.env.MYSQL_PASS !== "string") {
+      throw th(`MYSQL_PASS is undefined`);
+    }
+
+    log(`mysql-test.js: connecting to knex database: >${getDbName()}<`);
+
+    // const connection = require("knex")({
+    const connection = knex({
+      client: "mysql",
+      connection: {
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASS,
+        database: getDbName(),
+      },
+    });
+
+    const row = await connection.raw("show databases");
+
+    const databases = row[0];
+
+    log(databases);
+
+    connection.destroy();
+  } catch (e) {
+    log(`
+    
+Global catch:     
+    
+    `);
+
+    throw e;
+  }
+})();
